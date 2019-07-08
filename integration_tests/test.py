1ee27d33e6495edd52f6efdc41079058fc7ea63e
Gubin Dmitry <gub-dmit@bk.ru>
2019-07-08 15:59:19 +0700

    test module improved

--- integration_tests/test.py
+++ integration_tests/test.py
@@ -1,31 +1,30 @@
 import subprocess
 import os
 
-			
+            
 def consoleTest(command, testinput, testoutput):
-	process = subprocess.Popen(command.split(), stdout=subprocess.pipe, stdin=subprocess.pipe)
-	realoutput = process.communicate(self.input.encode("utf-8"))
-	return testoutput == realoutput[0].decode("utf-8")
+    try:
+        process = subprocess.Popen(command.split(), stdout=subprocess.pipe, stdin=subprocess.pipe)
+        realoutput = process.communicate(self.input.encode("utf-8"))
+    except:
+        print("Creating process error. Your command is " + command + ".")
+        return None
+    return testOutput == realOutput[0].decode("utf-8")
 
 def fileTest(command, filePath, fileContent = None, testInput = ""):
-	process = subprocess.Popen(command.split())
-	process.communicate(testInput.encode("utf-8"))
-	if os.path.exists(filePath):
-		if fileContent != None:
-			testFile = open(filePath, "r")
-			if testFile.read() == fileContent:
-				testFile.close()
-				#print(1)
-				return True
-			else:
-				testFile.close()
-				#print(2)
-				return False
-		#print(3)
-		return True
-	else:
-		#print(4)
-		return False
-
-
-print(fileTest("./upper", "testfilse.txt", "test\n")) #example
+    try:
+        process = subprocess.Popen(command.split())
+        process.communicate(testInput.encode("utf-8"))
+    except:
+        print("Creating process error. Your command is " + command + ".")
+        return None
+    if not  os.path.exists(filePath):
+        return False
+    if fileContent != None:
+        testFile = open(filePath, "r")
+        content = testFile.read()
+        testFile.close()
+        return content == fileContent
+    return True
+    
+print(fileTest("./upper", "testfile.txt", "test\n")) #example
