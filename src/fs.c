4f2fadd45518984c19b1bf15830b58b576c3e424
Gubin Dmitry <gub-dmit@bk.ru>
2019-07-09 11:36:41 +0700

    fs helper. added get_gg_root_path.

--- src/fs.c
+++ src/fs.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include "string_t.h"
+#include <string.h>
 #define MAX_PATH_LENGTH 1024
 #define BUF_SIZE 1024
 
@@ -22,6 +24,24 @@ int file_copy(const char *path_from, const char *path_to){
     fclose(file_from);
     return 1; //success 
 }
+
+int str_del_prelast_slash(char *string){
+    int last_index = -1;
+    int i = 0;
+    int index = -1;
+    while(string[i] != 0){
+        if (string[i] == '/'){
+            index = last_index;
+            last_index = i;
+        }
+        i++; 
+    }
+    if(index != -1){
+        string[index] = 0;
+    }
+    return index;
+}
+
 
 int file_delete(const char *path){
     return remove(path) == 0;
@@ -43,7 +63,7 @@ int is_file(const char *path){
     return (sb.st_mode & S_IFMT) == S_IFREG;
 }
 
-int is_directory(const char *path){
+int is_directory(char *path){
     struct stat sb;
     if (stat(path, &sb) == -1){
         return -1;
@@ -51,14 +71,18 @@ int is_directory(const char *path){
     return (sb.st_mode & S_IFMT) == S_IFDIR;
 }
 
-/*
 int get_gg_root_path(char *buf){
     if(getcwd(buf, MAX_PATH_LENGTH) == NULL){
         return -1;
     }
-    
-    if()
-    
+    while(strcmp("", buf) != 0){
+        strcat(buf, "/.gg");        
+        if(is_directory(buf) == 1){
+            return 1;
+        }
+        if(str_del_prelast_slash(buf) == -1){
+            break;
+        }
+    }
+    return -1; 
 }
-
-*/
