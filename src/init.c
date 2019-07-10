1073139ed4a8e173af956885be5740156cfdb935
Gubin Dmitry <gub-dmit@bk.ru>
2019-07-10 13:54:09 +0700

    made init function

---
+++ src/init.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "fs.h"
+#include <string.h>
+#include <unistd.h>
+#include "init.h"
+
+void sub_init(char *path, const char *folder){
+    //str_del_prelast_slash(strcat(path, "/"));
+    strcat(path, folder);
+    file_mkdir(path);
+    str_del_prelast_slash(strcat(path, "/"));
+}
+
+int init(){
+    char buf[MAX_PATH_LENGTH];
+    if(getcwd(buf, MAX_PATH_LENGTH) == NULL){
+        return 0;
+    }
+    strcat(buf, GG_DIR_NAME);
+    if(is_directory(buf) == 1){
+        return 0;
+    }
+    file_mkdir(buf);
+    sub_init(buf, "/objects");
+    return 1;
+}
+
+
